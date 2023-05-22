62250850865a0bc020369b4478d1187772f3a23d
gabriellaxdantas <gabriellaxdantas@gmail.com>
2023-05-22 13:07:58 -0300

    Roteiro 6

---
+++ Roteiro06/Exercicio01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Roteiro6Exercicio2
+{
+    class Program
+    {
+        static double somandoNums (double n1, double n2){
+             double soma;
+            soma =n1+n2;
+            return soma;
+        }
+        static void Main(string[] args)
+        {
+            double n1, n2, res=0;
+            Console.WriteLine("Informe dois números:");
+
+            n1=double.Parse(Console.ReadLine());
+            n2=double.Parse(Console.ReadLine());
+
+            res=somandoNums(n1, n2);
+            Console.WriteLine("Soma = " + res);
+        }
+    }
+}
---
+++ Roteiro06/Exercicio02/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Roteiro6Exercicio2
+{
+    class Program
+    {
+        static double mediaAri (double nota1, double nota2, double nota3){
+            double mediaA=0;
+            mediaA= (nota1 +nota2 +nota3) /3;
+            return mediaA;
+             }
+
+         static double mediaPon (double nota1, double nota2, double nota3){
+            double mediaP=0;
+            mediaP=(((nota1*2) + (nota2*3) +(nota3*5))/10);
+            return mediaP; 
+            }
+
+
+        static void Main(string[] args)
+        {
+            double nota1, nota2, nota3, res=0;
+            int opcao;
+            Console.WriteLine("Informe as notas:");
+            nota1=double.Parse(Console.ReadLine());
+            nota2=double.Parse(Console.ReadLine());
+            nota3=double.Parse(Console.ReadLine());
+
+            Console.WriteLine("1 para media aritimetica e 2 para media ponderada (outros valores serão desconsiderados)");
+            opcao=int.Parse(Console.ReadLine());
+            if(opcao == 1){
+               res= mediaAri(nota1, nota2, nota3);  
+            } 
+            else if (opcao == 2){
+                res= mediaPon(nota1, nota2, nota3);
+            }
+            else{
+                Console.WriteLine("Opção invalida");
+            }
+            Console.WriteLine(res);
+
+
+      }
+    }
+ }
+ 
+
+
