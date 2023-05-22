b32478933a649457779c4011e8c3487292d7eff6
gabriellaxdantas <gabriellaxdantas@gmail.com>
2023-05-22 15:03:03 -0300

    Roteiro 05

---
+++ Roteiro05/Exercicio01/Program.cs
@@ -0,0 +1,13 @@
+﻿int funcRec1 (int a){
+    if (a<=1){
+        return 0;
+    }
+    else {
+        return funcRec1((a-2) + 5);
+    }
+}
+
+
+int a = 6;
+int res = funcRec1(a);
+Console.WriteLine(res);
---
+++ Roteiro05/Exercicio03/Program.cs
@@ -0,0 +1,19 @@
+﻿int funcao (int a, int [] vetor){
+    if (a == 1){
+        return vetor[0];
+    }
+    else {
+        int x;
+        x = funcao(a-1, vetor);
+        if (x > vetor[a-1]){
+            return x;
+        }
+        else 
+        return vetor[ a-1];
+    }
+}
+
+int [] vetor = new int [7] {10, 5, 7, 15, 22, 9, 30};
+int a = 6;
+int res = funcao(a, vetor);
+Console.WriteLine(res);
