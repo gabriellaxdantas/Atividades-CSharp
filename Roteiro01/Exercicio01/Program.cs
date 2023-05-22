85055c6e215cc22bcc62291053958e1817215a67
gabriellaxdantas <gabriellaxdantas@gmail.com>
2023-05-22 12:18:35 -0300

    Roteiro 01

---
+++ Roteiro01/Exercicio01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Roteito_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primeiro Programa em C#");
+        }
+    }
+}
---
+++ Roteiro01/Exercicio02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Roteiro1Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, X;
+            Console.WriteLine("EXEMPLO 4 - Programa em C#");
+            X=int.Parse(Console.ReadLine();)
+            a=X/1000;
+            b = (X % 1000) / 100;
+            c = ((X % 1000) % 100)/ 10;
+            d = ((X % 1000) % 100) % 10;
+            Console.WriteLine("{0}{1}{2}{3}",d, c, b, a);
+            Console.WriteLine();
+            
+        }
+    }
+}
